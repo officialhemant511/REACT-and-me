38064507ae584fa3cdca9a5d831955a63e27a863
officialhemant511 <officialhemant511@gmail.com>
2024-03-21 20:04:03 +0530

    My first react poject setup

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+
+import Show from "./hema"
+function App() {
+  return (
+    <>
+    <h1>Hemant is a good boy | Hemant Bhatt</h1>
+    <Show/>
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/hema.jsx
@@ -0,0 +1,9 @@
+
+
+function Show(){
+    return (
+        <h2>M hu bhai</h2>
+    )
+}
+
+export default Show;
